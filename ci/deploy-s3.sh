31b92c0f0ecbb815e2b3b7f6bfd943b7738f27a5
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-11-12 22:25:56 +0000

    add logging output to deploy-s3.sh

--- ci/deploy-s3.sh
+++ ci/deploy-s3.sh
@@ -17,9 +17,13 @@ if [[ ! ${!DEPLOY_NAME[@]} ]]; then
   exit 1
 fi
 
+echo "Uploading ${BUILD_ARTIFACT} to ${S3_BUCKET}/${DEPLOY_NAME}"
 (cat "${BUILD_ARTIFACT}" | mc pipe deploy/${S3_BUCKET}/${DEPLOY_NAME}) 5>&1 | md5sum - > "${DEPLOY_NAME}.md5"
 
+echo "Uploaded with md5: $(cat {DEPLOY_NAME}.md5)"
+
 # fetch data back from s3 and check md5
+echo "checking md5 of ${S3_BUCKET}/${DEPLOY_NAME}"
 mc cat deploy/${S3_BUCKET}/${DEPLOY_NAME} | md5sum -c "${DEPLOY_NAME}.md5"
 
 echo "Uploaded ${DEPLOY_NAME} to ${S3_BUCKET} with md5: $(cat ${DEPLOY_NAME}.md5)"
